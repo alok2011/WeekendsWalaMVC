b48c759822a9e65565a511a9e5646147a7d4c60d
alok2011 <alokranjantiwaricse@gmail.com>
2024-07-11 16:47:14 +0530

    update

--- src/main/java/springmvc/io/WelcomeController.java
+++ src/main/java/springmvc/io/WelcomeController.java
@@ -2,6 +2,7 @@ package springmvc.io;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController 
@@ -10,7 +11,15 @@ public class WelcomeController
 	public String welcome()
 	{
 		System.out.println("WelcomeController.welcome()");
-		return "home.jsp";
+		return "home";
+	}
+	
+	@RequestMapping("/login")
+	@ResponseBody
+	public String login()
+	{
+		System.out.println("WelcomeController.welcome()");
+		return "Login Successfully Done!!!!!";
 	}
 
 }
