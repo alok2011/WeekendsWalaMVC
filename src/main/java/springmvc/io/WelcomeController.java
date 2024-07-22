8accce29039e2050d24989d275369d6afc4838ba
alok2011 <alokranjantiwaricse@gmail.com>
2024-07-22 15:29:20 +0530

    update

--- src/main/java/springmvc/io/WelcomeController.java
+++ src/main/java/springmvc/io/WelcomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -44,8 +45,6 @@ public class WelcomeController
 	}
 
 	
-	
-//	// hello/getCustomer/1234/details
 	@ResponseBody
 	@GetMapping(path = { "/getCustomer/{customerId}/details" })
 	public String getPathVariable(@PathVariable(name = "customerId") String customerId)
@@ -54,55 +53,72 @@ public class WelcomeController
 		System.out.println("Controller-  params=customerId : " + customerId);
 		return "Controller-  params=customerId : " + customerId;
 	}
-//
-//	
-//	
-//	
-//	@RequestMapping("/setCookie")
-//	public String setCookie(HttpServletResponse response) {
-//	    Cookie cookie = new Cookie("myCookie", "DangerValue");
-//	    cookie.setMaxAge(3600); // Cookie will expire in 1 hour
-//	    response.addCookie(cookie);
-//	    return "redirect:/electronics/second";
-//	}
-//	
-//	
-//	
-//	
-//	
-//	@RequestMapping("/second")
-//    public String secondPage(@CookieValue(value = "myCookie", defaultValue = "defaultCookieValue1") String myCookieValue, Model model) {
-//        // Use myCookieValue in your controller logic
-//        model.addAttribute("cookiesValue", myCookieValue);
-//        return "second";
-//    }
-//	
-//	
-//	@GetMapping(path = { "/getCookie" })
-//	public String getCookiesValue(HttpServletRequest req, @CookieValue("JSESSIONID") String jsessionCookie) {
-//		String value= "";
-//		Cookie[] cookies = req.getCookies();
-//		for (Cookie cookie : cookies) {
-//			if (cookie.getName().equals("JSESSIONID")) {
-//				value = cookie.getValue();
-//			}
-//		}
-//		System.out.println("Controller-  cookie=" + value);
-//		System.out.println("Controller-  jsessionCookie=" + jsessionCookie);
-//		return "index";
-//	}
-//
-//	// @RequestMapping(method = RequestMethod.POST)
+
+	@RequestMapping("/setCookie")
+	public String setCookie(HttpServletResponse response) {
+	    Cookie cookie = new Cookie("myCookie", "DangerValue");
+	    cookie.setMaxAge(10); // Cookie will expire in 10 seconds
+	    response.addCookie(cookie);
+	    return "redirect:/second";
+	}
+
+	
+	@RequestMapping("/second")
+//	@ResponseBody
+    public String secondPage(@CookieValue(value = "myCookie", defaultValue = "defaultCookieValue1") String myCookieValue, Model model) 
+	{
+        // Use myCookieValue in your controller logic
+        model.addAttribute("cookiesValue", myCookieValue);
+        return "second.jsp";
+    }
+
+	
+	@GetMapping(path = { "/getCookie" })
+	public String getCookiesValue(HttpServletRequest req, @CookieValue("JSESSIONID") String jsessionCookie) 
+	{
+		String value= "";
+		Cookie[] cookies = req.getCookies();
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals("JSESSIONID")) {
+				value = cookie.getValue();
+			}
+		}
+		System.out.println("Controller-  cookie=" + value);
+		System.out.println("Controller-  jsessionCookie=" + jsessionCookie);
+		return "index";
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+// @RequestMapping(method = RequestMethod.POST)
 //	@PostMapping
-//	public String post(HttpServletRequest req, HttpServletResponse res) {
+//	public String post(HttpServletRequest req, HttpServletResponse res)
+//	{
 //
 //		System.out.println("Controller- POST");
 //		return "index";
 //	}
-//
-//	// @RequestMapping(method = RequestMethod.DELETE)
+
+	
+// @RequestMapping(method = RequestMethod.DELETE)
 //	@DeleteMapping
-//	public String delete(HttpServletRequest req, HttpServletResponse res) {
+//	public String delete(HttpServletRequest req, HttpServletResponse res) 
+//	{
 //
 //		System.out.println("Controller- DELETE");
 //		return "index";
